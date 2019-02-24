521113350df2993b8edd0c3ba0ab52efe465c60f
karthik0@vt.edu <karthik0@vt.edu>
2019-02-23 19:08:38 -0700

    Allow for vectorized inputs, and fixed an error.

--- lla2ecef.py
+++ lla2ecef.py
@@ -4,11 +4,17 @@ import numpy as np
 def lla2ecef(lat, lon, alt):
 	a = 6378137
 	a_sq = a**2
-	e_sq = 6.69437999014e-3
+	e = 8.181919084261345e-2
+	e_sq = e**2
+	b_sq = a_sq*(1 - e_sq)
+
+	lat = np.array([lat]).reshape(np.array([lat]).shape[-1], 1)*np.pi/180
+	lon = np.array([lon]).reshape(np.array([lon]).shape[-1], 1)*np.pi/180
+	alt = np.array([alt]).reshape(np.array([alt]).shape[-1], 1)
 
 	N = a/np.sqrt(1 - e_sq*np.sin(lat)**2)
 	x = (N+alt)*np.cos(lat)*np.cos(lon)
 	y = (N+alt)*np.cos(lat)*np.sin(lon)
-	z = ((1-e_sq)*N+alt)*np.sin(lat)
+	z = ((b_sq/a_sq)*N+alt)*np.sin(lat)
 
 	return x, y, z
